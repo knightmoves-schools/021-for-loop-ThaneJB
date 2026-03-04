d76a52b110e7d9c837c398ff7c5cd1d7ae0f1c45
Thane <thabay0606@students.maquoketaschools.org>
2026-03-04 14:03:00 -0600

    I have no clue if this will work.

--- ToDo.cs
+++ ToDo.cs
@@ -2,4 +2,11 @@
 public class ToDo
 {
     // Add your code here
+    public string MarkAsDone(string todos){
+
+        for (int i = 0; i < todos.Length; i++){
+            todos[i] #1 = "done - " + todos[i];
+        }
+            return todos;
+    }
 }
