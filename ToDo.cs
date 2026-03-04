03491a34b6743b878fd14ab366b4303194294d24
Thane <thabay0606@students.maquoketaschools.org>
2026-03-04 14:05:09 -0600

    Removed #1

--- ToDo.cs
+++ ToDo.cs
@@ -5,7 +5,7 @@ public class ToDo
     public string MarkAsDone(string todos){
 
         for (int i = 0; i < todos.Length; i++){
-            todos[i] #1 = "done - " + todos[i];
+            todos[i] = "done - " + todos[i];
         }
             return todos;
     }
